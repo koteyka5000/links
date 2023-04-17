51666cad388ac05c6f0306488153fa3973e57d53
koteyka5000 <romacat5000@gmail.com>
2023-04-17 21:42:26 +0300

    file for metro site, more beautiful output

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import random  # Для генерации случайной ссылки
 from multiprocessing import Process  # Для работы в нескольких процессах одновременно
 from time import sleep
 from string import ascii_letters
+import datetime
 
 
 alpha = ascii_letters + '1234567890'
@@ -13,6 +14,7 @@ is_write = 1 # Записывать ли ссылку в файл
 string_len = 5 # Длинна строки для подбора
 max_combo_to_block = 2 # необходимое количество подряд открытых вкладок для аварийного завершения и предотвращения спама вкладками
 processes = 2 # Во сколько процессов работать (больше -> быстрее, но можно получить блокировку на сайте)
+
 
 def run():
     last = 0
@@ -41,11 +43,23 @@ def run():
                 sleep(5)
                 exit()
             print('^^^^^===^^^===========================================')
+            r = requests.get(f'{site}/{s}') # head не содержит переадресованной ссылки, а get содержит, но get работает медленнее
+            if 'https://cards.metro-cc.ru/' in r.url: # Если сайт это metro
+                print('==METRO==')
+                with open('urls_metro.txt', 'a') as f: # Чтобы не нагружать основной файл
+                    now = datetime.datetime.now()
+                    time = now.strftime("%d.%m %H:%M:%S")
+                    f.write(f'[{time}] -> {site}/{s} | {r.url}\n')
+                continue
+
+
             if is_open:
                 webbrowser.open(f'{site}/{s}', new=2)
             if is_write:
                 with open('urls', 'a') as f:
-                    f.write(f'{site}/{s}\n')
+                    now = datetime.datetime.now()
+                    time = now.strftime("%d.%m %H:%M:%S")
+                    f.write(f'[{time}] -> {site}/{s} | {r.url}\n')
             last += 1
         else:
             last = 0
@@ -54,6 +68,3 @@ if __name__ == '__main__':
     for w in range(processes):  
         p = Process(target=run)  
         p.start()
-
-
-
